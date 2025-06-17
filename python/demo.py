2386ab0e1232093f1f80474056f0641329f2654a
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-06-17 03:21:17 +0000

    working demo

--- python/demo.py
+++ python/demo.py
@@ -1,19 +1,21 @@
+import torch
+import numpy as np
+from PIL import Image
+import os
+
+from information_estimation import information_estimation
+from utils.process_depth import get_3d
 # Open yaml
 import yaml
 
 with open("nyu.yaml", "r") as file:
     config = yaml.safe_load(file)
-print(config)
 
 INTRINSICS = [config["fx"], config["fy"], config["cx"], config["cy"]]
 R = config["depth_max"]  # Maximum range of sensor
 EPSILON = config["resolution"]  # Resolution of the sensor
 
 # Image dir
-import torch
-import numpy as np
-from PIL import Image
-import os
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 DATA_DIR = "/scratchdata/nyu_plane"
@@ -24,34 +26,49 @@ rgb = np.array(rgb)
 
 depth = Image.open(os.path.join(DATA_DIR, "depth", f"{INDEX}.png")).convert("I")
 depth = np.array(depth) * EPSILON
-print(depth.max(), depth.min())
 
+pts_3d = get_3d(depth, INTRINSICS)
 # Tuning parameters
-
 
 TARGET_FOLDER = "sam"
 
 SIGMA_RATIO = 0.01
+SIGMA = SIGMA_RATIO * depth
 
 CONFIDENCE = 0.99
-INLIER_THRESHOLD = 0.1
-MAX_PLANE = 16
+INLIER_RATIO= 0.1
+MAX_PLANE = 9
 
-USE_SAM = True
+USE_SAM = False
 
 SAM_CONFIDENCE = 0.99
-SAM_INLIER_THRESHOLD = 0.2
+SAM_INLIER_RATIO = 0.2
 SAM_MAX_PLANE = 4
 
 POST_PROCESSING = False
 
+global_mask = np.zeros_like(depth, dtype=np.int32).flatten()
+global_planes = []
+
 # Use SAM to partition the image
 if USE_SAM: 
+    print("Using SAM to partition the image...")
+
     from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 
     sam = sam_model_registry["default"](checkpoint="/scratchdata/sam_vit_h_4b8939.pth").to(DEVICE)
     mask_generator = SamAutomaticMaskGenerator(sam, stability_score_thresh=0.98)
 
     sam_masks = mask_generator.generate(rgb)
-    print(len(sam_masks))
+    print("SAM of Regions:", len(sam_masks))
     masks = sorted(sam_masks, key=lambda x: x["stability_score"])
+
+
+valid_mask = (global_mask == 0) & (depth > 0).flatten()
+mask, plane = information_estimation(pts_3d, R, EPSILON, SIGMA.flatten(), CONFIDENCE, INLIER_RATIO, MAX_PLANE, valid_mask=valid_mask, verbose=True)
+
+print(mask.max())
+global_mask = np.where(mask > 0, mask+global_mask.max(), global_mask)
+print(global_mask.max())
+global_planes.append(plane)
+print(global_planes)
---
+++ python/information_estimation.py
@@ -0,0 +1,130 @@
+import numpy as np
+from tqdm import tqdm
+import time
+
+from utils.process_depth import get_3d
+
+def information_estimation(PTS_3D, R, EPSILON, SIGMA, CONFIDENCE=0.99, INLIER_RATIO=0.01, MAX_PLANE=1, valid_mask=None, verbose=False):
+    Z = PTS_3D[:, 2]
+    
+    assert(MAX_PLANE > 0), "MAX_PLANE must be greater than 0"
+
+    N = PTS_3D.shape[0]
+    if valid_mask is not None: TOTAL_NO_PTS = valid_mask.sum()
+    else: TOTAL_NO_PTS = N
+
+    mask = np.zeros(N, dtype=np.int32).flatten()
+
+    if TOTAL_NO_PTS < 3:
+        if verbose: print("Not enough points to fit a plane.")
+        return mask, []
+    
+    directional_vec = PTS_3D / (np.linalg.norm(PTS_3D, axis=1, keepdims=True) + 1e-16)
+    
+    SPACE_STATES = np.log(R/EPSILON)
+    PER_POINT_INFO = np.log(SIGMA + 1e-16) - np.log(EPSILON) + 0.5 * np.log(2*np.pi) - SPACE_STATES
+    TWO_SIGMA_SQUARE = 2 * SIGMA**2 + 1e-16
+
+    ITERATION = int(np.log(1 - CONFIDENCE) / np.log(1 - INLIER_RATIO**3))
+
+    information = np.full(MAX_PLANE+1, np.inf, dtype=float)
+    mask = np.zeros(N, dtype=int)
+    plane = np.zeros((MAX_PLANE+1, 4), dtype=float)
+    availability_mask = np.ones(N, dtype=bool)
+    if valid_mask is not None:
+        availability_mask = valid_mask
+
+    # O
+    information[0] = TOTAL_NO_PTS * SPACE_STATES
+
+    # nP + 0
+    for plane_cnt in range(1, MAX_PLANE+1):
+        available_index = np.linspace(0, N-1, N, dtype=int)
+        available_index = np.where(availability_mask)[0]
+
+        information[plane_cnt] =  information[plane_cnt-1]
+        information[plane_cnt] -= TOTAL_NO_PTS * np.log(plane_cnt) # Remove previous mask 
+        information[plane_cnt] += TOTAL_NO_PTS * np.log(plane_cnt+1) # New Mask that classify points
+        information[plane_cnt] += 3 * SPACE_STATES # New Plane
+
+        if (availability_mask).sum() < 3:
+            break
+
+        if verbose: start = time.time()
+        
+        BEST_INLIERS_MASK = np.zeros(N, dtype=bool)
+        BEST_ERROR = 0
+        BEST_PLANE = np.zeros(4, dtype=float)
+
+        for _ in tqdm(range(ITERATION), disable=not verbose):
+            # Get 3 random points
+            idx = np.random.choice(available_index, 3, replace=False)
+
+            # Get the normal vector and distance
+            A = PTS_3D[idx[0]]
+            B = PTS_3D[idx[1]]
+            C = PTS_3D[idx[2]]
+
+            AB = B - A
+            AC = C - A
+            normal = np.cross(AB, AC)
+            normal = normal / (np.linalg.norm(normal) + 1e-16)
+            distance = -np.dot(normal, A)            
+            
+            # Count the number of inliers
+            error = ((-distance/(np.dot(directional_vec, normal.T)+1e-16))*directional_vec[:,2] - Z) ** 2
+            error = error / TWO_SIGMA_SQUARE + PER_POINT_INFO
+            trial_mask = error < 0
+            trial_mask = trial_mask & availability_mask
+            trial_error = error[trial_mask].sum()
+
+            if  trial_error < BEST_ERROR:
+                
+                #SVD to find normal and distance
+                inliers = PTS_3D[trial_mask]
+                normal, distance = fit_plane(inliers)
+                error = ((-distance/(np.dot(directional_vec, normal.T)+1e-7))*directional_vec[:,2] - Z) ** 2
+                error = error / TWO_SIGMA_SQUARE + PER_POINT_INFO
+                trial_mask = error < 0
+                trial_mask = trial_mask & availability_mask
+                trial_error = error[trial_mask].sum()
+                
+                BEST_INLIERS_MASK = trial_mask
+                BEST_PLANE = np.concatenate((normal, [distance]))
+                BEST_ERROR = trial_error
+        
+        if verbose: print(time.time()-start)
+
+        information[plane_cnt] += BEST_ERROR
+        mask[BEST_INLIERS_MASK] = plane_cnt
+        plane[plane_cnt] = BEST_PLANE
+
+        availability_mask[BEST_INLIERS_MASK] = 0
+
+    min_idx = np.argmin(information)
+    if verbose: print("Min Planes: ", min_idx)
+    plane = plane[1:min_idx+1]
+    mask[mask > min_idx] = 0
+
+    return mask, plane
+
+def fit_plane(points):
+    # Compute the centroid (mean) of the points
+    centroid = np.mean(points, axis=0)
+    
+    # Shift the points to the centroid
+    shifted_points = points - centroid
+    
+    # Compute the covariance matrix
+    cov_matrix = np.dot(shifted_points.T, shifted_points) / len(points)
+    
+    # Perform eigenvalue decomposition
+    eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+    
+    # The normal vector is the eigenvector corresponding to the smallest eigenvalue
+    normal_vector = eigenvectors[:, np.argmin(eigenvalues)]
+    
+    # The equation of the plane is normal_vector . (x - centroid) = 0
+    D = -np.dot(normal_vector, centroid)
+    
+    return normal_vector, D
