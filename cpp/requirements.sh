c228c8b49f1843b7aa39f3ecdb532a8fb14a76bd
tauzn-clock <zhongdaoxin1999@gmail.com>
2025-06-23 11:12:17 +0000

    requirements for cpp

---
+++ cpp/requirements.sh
@@ -0,0 +1,8 @@
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y cmake
+apt-get install -y libopencv-dev
+apt-get install -y libyaml-cpp-dev
+apt-get install -y libboost-all-dev
+apt-get install -y libeigen3-dev
